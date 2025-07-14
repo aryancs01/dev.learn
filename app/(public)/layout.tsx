b75ea5dc49ab1a64093a739193fe762634bd3d44
Aryan Saxena <aryansaxenacs@gmail.com>
2025-07-14 20:37:41 +0530

    get course public route

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -4,6 +4,6 @@ import { Navbar } from "./_components/Navbar";
 export default function LayoutPublic({children}:{children:ReactNode}){
     return <div>
         <Navbar/>
-        <main className="container mx-auto px-4 md:px-6 lg:px-8">{children}</main>
+        <main className="container mx-auto px-4 md:px-6 lg:px-8 mb-32">{children}</main>
     </div>
 }
